e85a9dbd27b33a110671439312c6840b9037d9c3
Emanuel Strömgren <emanuel.stromgren@piktiv.se>
2021-12-06 09:49:48 +0100

    Added solution for day 1b (code stuck on work computer over the weekend...)

--- src/day_1.rs
+++ src/day_1.rs
@@ -1,6 +1,7 @@
 
 pub fn solve_day_1a(lines: impl Iterator<Item = String>) -> u32 {
-    let mut numbers = lines.map(|l| l.parse::<i32>().unwrap());
+    let mut numbers = lines
+        .filter_map(|l| l.parse::<i32>().ok());
     let mut previous = numbers.next().unwrap();
 
     let mut count = 0;
@@ -14,7 +15,18 @@ pub fn solve_day_1a(lines: impl Iterator<Item = String>) -> u32 {
 }
 
 pub fn solve_day_1b(lines: impl Iterator<Item = String>) -> u32 {
-    let mut numbers = lines.map(|l| l.parse::<i32>().unwrap());
+    let numbers: Vec<i32> = lines
+        .filter_map(|l| l.parse::<i32>().ok())
+        .collect();
+    let mut sums = numbers.windows(3).map(|i| i.iter().sum());
+    let mut previous: i32 = sums.next().unwrap();
 
-    0
+    let mut count = 0;
+    for value in sums {
+        if previous < value {
+            count += 1;
+        }
+        previous = value;
+    }
+    count
 }
