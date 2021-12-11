f3250a8f23d9252086b6ed29e6ac61f8a48cf332
Emanuel Strömgren <emanuel.stromgren@pulpeten.se>
2021-12-11 16:20:39 +0100

    Solution for day 11

---
+++ src/day_11.rs
@@ -0,0 +1,79 @@
+use std::cmp::{max, min};
+
+fn flash(energy_levels: &mut [u8; 10*10], x: usize, y: usize) -> u32 {
+    let mut flashes = 1;
+    energy_levels[y * 10 + x] = 0;
+    for y in (max(y as i32 - 1, 0) as usize)..min(y + 2, 10) {
+        for x in (max(x as i32 - 1, 0) as usize)..min(x + 2, 10) {
+            match energy_levels[y * 10 + x] {
+                0 => continue,
+                9 => flashes += flash(energy_levels, x, y),
+                _ => energy_levels[y * 10 + x] += 1,
+            }
+        }
+    }
+
+    energy_levels[y * 10 + x] = 0;
+    flashes
+}
+
+pub fn solve_day_11a(input: &String) -> u32 {
+    let mut energy_levels = [0; 10 * 10];
+    input.lines().enumerate().for_each(|(y, line)| {
+        for (x, level) in line.as_bytes().iter().enumerate() {
+            energy_levels[y * 10 + x] = *level - '0' as u8;
+        }
+    });
+
+    let mut flashes = 0;
+    for i in 0..100 {
+        for y in 0..10 {
+            for x in 0..10 {
+                energy_levels[y * 10 + x] += 1;
+            }
+        }
+
+        for y in 0..10 {
+            for x in 0..10 {
+                if energy_levels[y * 10 + x] > 9 {
+                    flashes += flash(&mut energy_levels, x, y);
+                }
+            }
+        }
+
+    }
+
+    flashes
+}
+
+pub fn solve_day_11b(input: &String) -> u32 {
+    let mut energy_levels = [0; 10 * 10];
+    input.lines().enumerate().for_each(|(y, line)| {
+        for (x, level) in line.as_bytes().iter().enumerate() {
+            energy_levels[y * 10 + x] = *level - '0' as u8;
+        }
+    });
+
+    let mut i = 0;
+    loop {
+        i += 1;
+        for y in 0..10 {
+            for x in 0..10 {
+                energy_levels[y * 10 + x] += 1;
+            }
+        }
+
+        let mut flashes = 0;
+        for y in 0..10 {
+            for x in 0..10 {
+                if energy_levels[y * 10 + x] > 9 {
+                    flashes += flash(&mut energy_levels, x, y);
+                }
+            }
+        }
+
+        if flashes == 100 {
+            return i;
+        }
+    }
+}
