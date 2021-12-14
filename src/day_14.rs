9d6816038dbc82ad08d8f816893f9266952eee85
Emanuel Strömgren <emanuel.stromgren@piktiv.se>
2021-12-14 15:34:26 +0100

    Partial solution for day 14

---
+++ src/day_14.rs
@@ -0,0 +1,47 @@
+use std::{fmt::Write, collections::HashMap};
+
+pub fn solve_a(input: &String, output: &mut String) {
+    let mut input = input.lines();
+    let mut read_text = input.next().unwrap().to_string();
+
+    let insertion_set: HashMap<&str, char> = HashMap::from_iter(
+        input.skip(1).map(|line | {
+            let mut line = line.split(" -> ");
+            let slice_match = line.next().unwrap();
+            let insert_char = line.next().unwrap().chars().next().unwrap();
+
+            (slice_match, insert_char)
+        })
+    );
+
+    {
+        let mut write_text = String::new();
+        for _ in 0..10 {
+            for i in 0..read_text.len()-1 {
+                write_text.write_char(read_text.as_bytes()[i] as char).unwrap();
+                if let Some(insert_char) = insertion_set.get(&read_text[i..i+2]) {
+                    write_text.write_char(*insert_char).unwrap();
+                }
+            }
+            read_text.clear();
+            read_text.write_str(write_text.as_str()).unwrap();
+            write_text.clear();
+        }
+    }
+
+    const OFFSET: usize = 'A' as usize;
+    const SIZE: usize = ('Z' as u8 - 'A' as u8) as usize;
+    let mut frequency_table: [u32; SIZE] = [0; SIZE];
+    for byte in read_text.as_bytes() {
+        frequency_table[*byte as usize - OFFSET] += 1;
+    }
+    writeln!(output, "\n{:?}", frequency_table).unwrap();
+
+    let max_count = frequency_table.iter().max().unwrap();
+    let min_count = frequency_table.iter().filter(|v| **v > 0).min().unwrap();
+
+    write!(output, "{}", max_count - min_count).unwrap();
+}
+
+pub fn solve_b(input: &String, output: &mut String) {
+}
--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ mod day_10;
 mod day_11;
 mod day_12;
 mod day_13;
+mod day_14;
 
 fn main() {
     let (iterations, days) = read_args();
@@ -76,6 +77,7 @@ fn solve(day: u32, input: &String, output: &mut String) {
         11 => solve_and_print_day!(day_11, 11, input, output),
         12 => solve_and_print_day!(day_12, 12, input, output),
         13 => solve_and_print_day!(day_13, 13, input, output),
+        14 => solve_and_print_day!(day_14, 14, input, output),
         _ => panic!("Could not solve day {}", day),
     };
 }
