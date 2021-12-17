8d8fd5c0c0151e349d876b1d8783757308be06b5
Emanuel Strömgren <emanuel.stromgren@piktiv.se>
2021-12-17 17:13:09 +0100

    Correct solution for 17 a, incorrect for b

--- src/day_17.rs
+++ src/day_17.rs
@@ -1,11 +1,18 @@
-use std::{fmt::Write, cmp::max};
-
-
-fn position_at_time(start_velocity: i32, constant_force: i32, time: i32) -> i32 {
-    start_velocity * time + ((constant_force * time * time) as f32 * 0.5f32).round() as i32
-}
+use std::fmt::Write;
 
 pub fn solve_a(input: &String, output: &mut String) {
+    let mut ranges = input["target area: x=".len()..input.len()].split(", y=");
+    ranges.next();
+    let mut range_y = ranges.next().unwrap().split("..");
+    let y_min: i32 = range_y.next().unwrap().parse().unwrap();
+
+    let initial_velocity = y_min.abs() - 1;
+    let top_position = initial_velocity * (initial_velocity + 1) / 2;
+
+    write!(output, "{}", top_position).unwrap();
+}
+
+pub fn solve_b(input: &String, output: &mut String) {
     let mut ranges = input["target area: x=".len()..input.len()].split(", y=");
     let mut range_x = ranges.next().unwrap().split("..");
     let mut range_y = ranges.next().unwrap().split("..");
@@ -16,28 +23,14 @@ pub fn solve_a(input: &String, output: &mut String) {
     let y_min: i32 = range_y.next().unwrap().parse().unwrap();
     let y_max: i32 = range_y.next().unwrap().parse().unwrap();
 
-    let initial_velocity = -y_min - 1;
-    let top_time_step = initial_velocity;
-    let final_time_step = initial_velocity * 2 + 1;
+    let x_min_initial_velocity = ((x_min * 2) as f32).sqrt().floor() as i32;
+    let x_max_initial_velocity = ((x_max * 2) as f32).sqrt().floor() as i32;
 
-    let top_position = position_at_time(initial_velocity, -1, top_time_step);
-    let final_position = position_at_time(initial_velocity, -1, final_time_step);
+    let y_min_initial_velocity = ((y_max.abs() * 2) as f32).sqrt().floor() as i32 * y_max.signum();
+    let y_max_initial_velocity = y_min.abs() - 1;
 
-    let mut position = 0;
-    let mut velocity = initial_velocity;
-    for _ in 0..top_time_step {
-        position += velocity;
-        velocity -= 1;
+    let result = (x_max_initial_velocity - x_min_initial_velocity) * (y_max_initial_velocity - y_min_initial_velocity);
 
-        println!("{}", position);
-    }
-
-    println!("Initial velocity: {}, top position: {} (at step {}), final position: {} (at step {})",
-        initial_velocity, top_position, top_time_step, final_position, final_time_step);
-
-    write!(output, "{}", top_position).unwrap();
+    write!(output, "{}", result).unwrap();
 }
 
-pub fn solve_b(input: &String, output: &mut String) {
-    write!(output, "{}", 0).unwrap();
-}
