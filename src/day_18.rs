a6e7ecb90c495ec17008cc082b4d390253bbfc88
Emanuel Strömgren <emanuel.stromgren@piktiv.se>
2021-12-20 18:04:59 +0100

    Broken sday 18, but some valid code at least...

--- src/day_18.rs
+++ src/day_18.rs
@@ -1,70 +1,203 @@
 use std::fmt::{Write, Display};
-use std::ops::Add;
-use std::rc::Rc;
 
-#[derive(Clone)]
-enum Element {
-    Pair(Box<Pair>),
-    Value(u8),
+#[derive(Clone, Copy)]
+enum Element<Pair = usize, Value = usize> {
+    Pair(Pair),
+    Value(Value),
 }
 
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 struct Pair {
+    parent: Option<usize>,
     left: Element,
     right: Element,
 }
 
-fn make_pair(left: u8, right: u8) -> Pair {
-    Pair {
-        left: Element::Value(left),
-        right: Element::Value(right),
-    }
+struct Hierarchy {
+    values: Vec<u8>,
+    pairs: Vec<Pair>,
 }
 
-impl Add for Pair {
-    type Output = Pair;
+fn reduce_explode(pair_index: usize, hierarchy: &mut Hierarchy, depth: u8) -> bool {
+    let pair = hierarchy.pairs[pair_index];
+    if depth < 4 {
+        let element_indices = [pair.left, pair.right];
+        for i in 0..2 {
+            let element_index = element_indices[i];
+            if let Element::Pair(pair_index) = element_index {
+                if reduce_explode(pair_index, hierarchy, depth + 1) {
+                    return true;
+                }
+            }
+        }
+    } else {
+        if let Element::Value(left_index) = pair.left {
+            if left_index > 0 {
+                hierarchy.values[left_index - 1] += hierarchy.values[left_index];
+            }
+            hierarchy.values[left_index] = 0;
 
-    fn add(self, rhs: Self) -> Self::Output {
-        Pair {
-            left: Element::Pair(Box::new(self)),
-            right: Element::Pair(Box::new(rhs)),
+            if let Some(parent_index) = pair.parent {
+                if let Element::Pair(index) = hierarchy.pairs[parent_index].left {
+                    if index == pair_index {
+                        hierarchy.pairs[parent_index].left = Element::Value(left_index);
+                    }
+                }
+                if let Element::Pair(index) = hierarchy.pairs[parent_index].right {
+                    if index == pair_index {
+                        hierarchy.pairs[parent_index].right = Element::Value(left_index);
+                    }
+                }
+            }
+        }
+
+        if let Element::Value(right_index) = pair.right {
+            if right_index < hierarchy.values.len() - 1 {
+                hierarchy.values[right_index + 1] += hierarchy.values[right_index];
+            }
+            hierarchy.values.remove(right_index);
+            for i in 0..hierarchy.pairs.len() {
+                if let Element::Value(index) = hierarchy.pairs[i].left {
+                    if index > right_index {
+                        hierarchy.pairs[i].left = Element::Value(index - 1);
+                    }
+                }
+                if let Element::Value(index) = hierarchy.pairs[i].right {
+                    if index > right_index {
+                        hierarchy.pairs[i].right = Element::Value(index - 1);
+                    }
+                }
+            }
+        }
+
+        hierarchy.pairs.remove(pair_index);
+        for i in 0..hierarchy.pairs.len() {
+            if let Element::Pair(index) = hierarchy.pairs[i].left {
+                if index > pair_index {
+                    hierarchy.pairs[i].left = Element::Pair(index - 1);
+                }
+            }
+            if let Element::Pair(index) = hierarchy.pairs[i].right {
+                if index > pair_index {
+                    hierarchy.pairs[i].right = Element::Pair(index - 1);
+                }
+            }
+        }
+
+        return true;
+    }
+    false
+}
+
+fn reduce_split(pair_index: usize, hierarchy: &mut Hierarchy) -> bool {
+    let element_indices = [hierarchy.pairs[pair_index].left, hierarchy.pairs[pair_index].right];
+    for i in 0..2 {
+        let element = element_indices[i];
+        match element {
+            Element::Pair(element_pair_index) => {
+                if reduce_split(element_pair_index, hierarchy) {
+                    return true;
+                }
+            }
+            Element::Value(value_index) => {
+                let value = hierarchy.values[value_index];
+                if value >= 10 {
+                    let left_value = value / 2;
+                    let right_value = value - left_value;
+
+                    let left_value_index = value_index;
+                    let right_value_index = value_index + 1;
+                    hierarchy.values[left_value_index] = left_value;
+                    hierarchy.values.insert(right_value_index, right_value);
+
+                    hierarchy.pairs.push(Pair {
+                        parent: Some(pair_index),
+                        left: Element::Value(left_value_index),
+                        right: Element::Value(right_value_index),
+                    });
+                    return true;
+                }
+            }
         }
     }
+    false
 }
 
-impl Add<u8> for Pair {
-    type Output = Pair;
-
-    fn add(self, rhs: u8) -> Self::Output {
-        Pair {
-            left: Element::Pair(Box::new(self)),
-            right: Element::Value(rhs),
+fn reduce(hierarchy: &mut Hierarchy) {
+    loop {
+        if reduce_explode(0, hierarchy, 0) {
+            continue;
         }
+
+        if reduce_split(0, hierarchy) {
+            continue;
+        }
+
+        break;
     }
 }
 
-impl Add<Pair> for u8 {
-    type Output = Pair;
+fn combine(left: Hierarchy, right: Hierarchy) -> Hierarchy {
+    let mut hierarchy = Hierarchy {
+        pairs: Vec::with_capacity(1 + left.pairs.len() + right.pairs.len()),
+        values: left.values,
+    };
+    let right_value_offest = hierarchy.values.len();
+    for value in right.values {
+        hierarchy.values.push(value);
+    }
 
-    fn add(self, rhs: Pair) -> Self::Output {
-        Pair {
-            left: Element::Value(self),
-            right: Element::Pair(Box::new(rhs)),
+    let left_pair_offset = 1;
+    let right_pair_offset = left.pairs.len() + 1;
+    hierarchy.pairs.push(Pair {
+        parent: None,
+        left: Element::Pair(left_pair_offset),
+        right: Element::Pair(right_pair_offset),
+    });
+
+    for (pairs, pair_offset, value_offset) in [
+        (left.pairs, left_pair_offset, 0),
+        (right.pairs, right_pair_offset, right_value_offest)
+    ] {
+        for pair in pairs {
+            hierarchy.pairs.push(Pair {
+                parent: match pair.parent {
+                    Some(parent_index) => Some(parent_index + pair_offset),
+                    None => Some(0),
+                },
+                left: match pair.left {
+                    Element::Pair(pair_index) => Element::Pair(pair_index + pair_offset),
+                    Element::Value(value_index) => Element::Value(value_index + value_offset),
+                },
+                right: match pair.right {
+                    Element::Pair(pair_index) => Element::Pair(pair_index + pair_offset),
+                    Element::Value(value_index) => Element::Value(value_index + value_offset),
+                },
+            });
         }
     }
+
+
+    hierarchy
 }
 
-impl Display for Pair {
+impl Display for Hierarchy {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.pairs[0].fmt(self, f)
+    }
+}
+
+impl Pair {
+    fn fmt(&self, hierarchy: &Hierarchy, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_char('[')?;
         match &self.left {
-            Element::Pair(pair) => pair.fmt(f),
-            Element::Value(value) => value.fmt(f),
+            Element::Pair(pair) => hierarchy.pairs[*pair].fmt(hierarchy, f),
+            Element::Value(value) => hierarchy.values[*value].fmt(f),
         }?;
         f.write_char(',')?;
         match &self.right {
-            Element::Pair(pair) => pair.fmt(f),
-            Element::Value(value) => value.fmt(f),
+            Element::Pair(pair) => hierarchy.pairs[*pair].fmt(hierarchy, f),
+            Element::Value(value) => hierarchy.values[*value].fmt(f),
         }?;
         f.write_char(']')
     }
@@ -75,8 +208,13 @@ enum Side {
     Right,
 }
 
-fn parse_line(line: &str) -> Pair {
-    let mut pair_stack: Vec<(Pair, Side)> = Vec::new();
+fn parse_line(line: &str) -> Hierarchy {
+    let mut hierarchy = Hierarchy {
+        pairs: Vec::new(),
+        values: Vec::new(),
+    };
+    let mut current_pair_index = 0;
+    let mut side = Side::Left;
 
     const PAIR_START: u8 = '[' as u8;
     const PAIR_END: u8 = ']' as u8;
@@ -85,46 +223,67 @@ fn parse_line(line: &str) -> Pair {
     for c in line.as_bytes().iter() {
         match *c {
             PAIR_START => {
-                pair_stack.push((Pair {
-                    left: Element::Value(0),
-                    right: Element::Value(0),
-                }, Side::Left));
+                let child_pair_index = hierarchy.pairs.len();
+                if current_pair_index >= child_pair_index {
+                    // Root
+                    hierarchy.pairs.push(Pair {
+                        parent: None,
+                        left: Element::Value(0),
+                        right: Element::Value(0),
+                    });
+                } else {
+                    hierarchy.pairs.push(Pair {
+                        parent: Some(current_pair_index),
+                        left: Element::Value(0),
+                        right: Element::Value(0),
+                    });
+                    let child_element = Element::Pair(child_pair_index);
+                    match side {
+                        Side::Left => hierarchy.pairs[current_pair_index].left = child_element,
+                        Side::Right => hierarchy.pairs[current_pair_index].right = child_element,
+                    }
+                }
+                side = Side::Left;
+                current_pair_index = child_pair_index;
             },
             PAIR_END => {
-                let (closed_pair, _) = pair_stack.pop().unwrap();
-                if let Some((top_pair, side)) = pair_stack.last_mut() {
-                    let element = Element::Pair(Box::new(closed_pair));
-                    match side {
-                        Side::Left => top_pair.left = element,
-                        Side::Right => top_pair.right = element,
-                    }
+                if let Some(parent_index) = hierarchy.pairs.last().unwrap().parent {
+                    current_pair_index = parent_index;
                 } else {
-                    return closed_pair;
+                    break;
                 }
             },
             PAIR_SEPARATOR => {
-                pair_stack.last_mut().unwrap().1 = Side::Right;
+                side = Side::Right;
             },
             _ => {
-                let value = Element::Value(*c - '0' as u8);
-                let (pair, side) = pair_stack.last_mut().unwrap();
+                let value = *c - '0' as u8;
+                let value_index = Element::Value(hierarchy.values.len());
+                hierarchy.values.push(value);
+
                 match side {
-                    Side::Left => pair.left = value,
-                    Side::Right => pair.right = value,
+                    Side::Left => hierarchy.pairs[current_pair_index].left = value_index,
+                    Side::Right => hierarchy.pairs[current_pair_index].right = value_index,
                 }
             },
         };
-    }
+    };
 
-    pair_stack.pop().unwrap().0
+    hierarchy
 }
 
 pub fn solve_a(input: &String, output: &mut String) {
-    let pairs = input.lines().map(parse_line);
+    let mut hierarchy = parse_line("[[[[[9,8],1],2],3],4]");
+    reduce(&mut hierarchy);
+    println!("{}", hierarchy);
 
-    for pair in pairs {
-        println!("{}", pair);
-    }
+
+
+    // let hierarchies = input.lines().map(parse_line);
+
+    // for hierarchy in hierarchies {
+    //     println!("{}", hierarchy);
+    // }
 
     write!(output, "{}", 0).unwrap();
 }
